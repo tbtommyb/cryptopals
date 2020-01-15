68e5af18390bd15fadd21382f71ce4aa1a1f01db
Tom Johnson <tom@tmjohnson.co.uk>
2020-01-15 12:32:35 +0000

    Add OverloadedStrings

--- src/Basics/Basics.hs
+++ src/Basics/Basics.hs
@@ -1,8 +1,9 @@
-module Basics.Basics (toBase64) where
+module Basics.Basics (hexToBase64) where
 
 import qualified Data.ByteString.Base64 as B64
 import qualified Data.ByteString.Base16 as B16
 import qualified Data.ByteString as B
 
-toBase64 :: B.ByteString -> B.ByteString
-toBase64 = B64.encode . fst . B16.decode
+hexToBase64 :: B.ByteString -> B.ByteString
+hexToBase64 = B64.encode . fst . B16.decode
+
--- test/Basics/BasicsSpec.hs
+++ test/Basics/BasicsSpec.hs
@@ -1,6 +1,7 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Basics.BasicsSpec (spec) where
 
-import qualified Data.ByteString.UTF8 as BSU
 import Test.Hspec
 import Basics.Basics
 
@@ -8,4 +9,4 @@ spec :: Spec
 spec = do
   describe "toBase64" $ do
     it "generates the correct base64 value" $ do
-      toBase64 (BSU.fromString "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d") `shouldBe` BSU.fromString "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+      hexToBase64 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d" `shouldBe` "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
